2f38ddae087d4a2f9670c44453c723d7f8df56ad
Azraei Hashim <mypc@Azraeis-MBP.realtek>
2023-03-09 18:50:49 +0800

    initial

---
+++ js/script.js
@@ -0,0 +1,92 @@
+'use strict';
+
+/**
+ * element toggle function
+ */
+
+const elemToggleFunc = function (elem) { elem.classList.toggle("active"); }
+
+
+
+/**
+ *  navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+const navElemArr = [overlay, navCloseBtn, navOpenBtn];
+
+
+
+/**
+ * close navbar when click on any navbar link
+ */
+
+for (let i = 0; i < navbarLinks.length; i++) { navElemArr.push(navbarLinks[i]); }
+
+
+
+/**
+ * add event on all elements for toggling navbar
+ */
+
+for (let i = 0; i < navElemArr.length; i++) {
+    navElemArr[i].addEventListener("click", function () {
+        elemToggleFunc(navbar);
+        elemToggleFunc(overlay);
+    });
+}
+
+
+
+/**
+ * header active state
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+    window.scrollY >= 400 ? header.classList.add("active")
+        : header.classList.remove("active");
+});
+
+
+
+/**
+ * countdown timer
+ */
+
+// Set the date we're counting down to
+var countDownDate = new Date("Mar 31, 2023 23:59:59").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Calculate days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with the corresponding ID
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  // If the countdown is finished, display a message
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
